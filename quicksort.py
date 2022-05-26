9b411388ab23346631d653529f49059be17abd21
Anton Durandin <enitefall81@gmail.com>
2022-05-26 10:23:27 -0700

    Quicksort

--- quicksort.py
+++ quicksort.py
@@ -1,2 +1,17 @@
+def quicksort(list):
+    if len(list) <= 1:
+        return list
+    else:
+        pivot = list.pop()
 
 
+    items_greater = []
+    items_lower = []
+    for item in list:
+        if item > pivot:
+           items_greater.append(item)
+        else:
+             items_lower.append(item)
+    return quicksort(items_lower) + [pivot] + quicksort(items_greater)
+
+print(quicksort([1,2,3,4,3,3,4,5,6,7]))
