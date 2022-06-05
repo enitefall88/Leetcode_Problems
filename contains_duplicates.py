59ce8884f3063916f1f3b0ae6c07fc91015fcf75
Anton Durandin <enitefall81@gmail.com>
2022-06-05 11:33:42 -0700

    Contains duplicates

---
+++ contains_duplicates.py
@@ -0,0 +1,12 @@
+nums = [1,2,3,1]
+
+
+def containsDuplicate(nums: [int]) -> bool:
+    hashset = set()
+    for n in nums:
+        if n in hashset:
+            return True
+        else:
+            hashset.add(n)
+
+print(containsDuplicate(nums))
--- quicksort.py
+++ quicksort.py
@@ -15,3 +15,20 @@ def quicksort(list):
     return quicksort(items_lower) + [pivot] + quicksort(items_greater)
 
 print(quicksort([1,2,3,4,3,3,4,5,6,7]))
+
+
+def quicksort2(list):
+    if len(list) <= 1:
+        return list
+    else:
+        pivot = list.pop()
+    items_greater = []
+    items_smaller = []
+    for i in list:
+        if list[i] < pivot:
+            items_smaller.append(i)
+        else:
+            items_greater.append(i)
+    return quicksort(items_smaller) + [pivot] + quicksort(items_greater)
+
+print(quicksort2([1,2,3,4,3,3,4,5,6,7]))
