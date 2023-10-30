e361761c717de9bed4b6ba965ccc1b28506206ec
Anton Durandin <enitefall81@gmail.com>
2023-10-30 10:22:31 -0700

    ss

--- selection_sort.py
+++ selection_sort.py
@@ -1,28 +1,4 @@
-# def findSmallest(arr):
-#     # Stores the smallest value
-#     smallest = arr[0]
-#     # Stores the index of the smallest value
-#     smallest_index = 0
-#     for i in range(1, len(arr)):
-#         if arr[i] < smallest:
-#             smallest_index = i
-#             smallest = arr[i]
-#     return smallest
-#
-#
-# print(findSmallest(arr=[4, 5, 6, 7, 3, 1]))
-#
-#
-# def selectionSort(arr):
-#     resultArray = []
-#     for i in range(len(arr)):
-#         smallest = findSmallest(arr)
-#         resultArray.append(arr.pop(smallest))
-#     return resultArray
-#
-#
-# print(selectionSort([4, 5, 6, 7, 3]))
-#
+
 
 
 
